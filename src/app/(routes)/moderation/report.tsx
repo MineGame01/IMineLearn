199b584748a32df125d173c760acb9704713fd8d
MineGame01 <klychka111@gmail.com>
2025-04-09 20:57:22 +0300

    UI feat: display id and content type with complaint

--- src/app/(routes)/moderation/report.tsx
+++ src/app/(routes)/moderation/report.tsx
@@ -6,7 +6,15 @@ import { useDeleteReportMutation } from '@app/api';
 import { getServerErrorMessage } from '@shared/model';
 import Link from 'next/link';
 
-export const Report: FC<IReport> = ({ _id, user_id, content, reason, created_at }) => {
+export const Report: FC<IReport> = ({
+  _id,
+  user_id,
+  content,
+  reason,
+  target_id,
+  target_type,
+  created_at,
+}) => {
   const [deleteReport, { isLoading, isError, error }] = useDeleteReportMutation();
 
   const errorMessage = getServerErrorMessage(error);
@@ -19,11 +27,12 @@ export const Report: FC<IReport> = ({ _id, user_id, content, reason, created_at 
     <Paper className="p-3">
       <div>Id: {_id}</div>
       <div>
-        User id:{' '}
-        <Link className="border-b-2 border-accent" href={`/user/${user_id}`}>
-          {user_id}
-        </Link>
+        User id: <Link href={`/user/${user_id}`}>{user_id}</Link>
       </div>
+      <div>
+        Target Id: <Link href={`/topic/${target_id}`}>{target_id}</Link>
+      </div>
+      <div>Target Type: {target_type}</div>
       <div>Reason: {reason}</div>
       <div>Content: {content}</div>
       <div>Created at: {dayjs(created_at).format('DD/MM/YYYY')}</div>
