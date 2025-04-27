7901e8718292d903b7723a7838f10c97bc6711a4
MineGame01 <klychka111@gmail.com>
2025-04-27 19:36:15 +0300

    feat: automatically close after authorization

--- src/widgets/LoginModal/ui/content-modal.tsx
+++ src/widgets/LoginModal/ui/content-modal.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { GreetingUser } from '@widgets/LoginModal/ui/greeting-user';
 import { AuthForm } from '@widgets/LoginModal/ui/auth-form';
 import { ButtonChangeTypeAuth } from '@widgets/LoginModal/ui/button-change-type-auth';
@@ -7,9 +7,22 @@ import { TTypeAuth } from '@widgets/LoginModal/model/TTypeAuth.ts';
 import * as m from 'motion/react-m';
 import { AnimatePresence } from 'motion/react';
 import { AppLogo } from '@shared/ui';
+import { useAppSelector } from '@app/lib';
+import { selectAuthAccessToken } from '../model/authSlice';
 
-export const ContentModal: FC = () => {
+interface IProps {
+  onClose: () => void;
+}
+
+export const ContentModal: FC<IProps> = ({ onClose }) => {
   const [typeAuth, setTypeAuth] = useState<TTypeAuth>('login');
+  const authAccessToken = useAppSelector(selectAuthAccessToken);
+
+  useEffect(() => {
+    if (authAccessToken) {
+      onClose();
+    }
+  }, [authAccessToken]);
 
   return (
     <div className="p-5">
